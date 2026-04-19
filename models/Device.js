914dd5c3375cd047f01bd1e6b70478779dce8623
Leoruiz197 <ruizruizleo@hotmail.com>
2026-04-19 12:01:32 -0300

    ajuste de model

--- models/Device.js
+++ models/Device.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const deviceSchema = new mongoose.Schema({
     deviceId: { type: String, unique: true },
-    status: { type: String, default: 'UNKNOWN' },
+    state: { type: Object, default: {} },
     lastCommand: { type: Array, default: [] },
     lastSeen: { type: Date }
 });
