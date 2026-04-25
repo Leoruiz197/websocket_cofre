3702436f42279ef63f9932530c7e4811f26f8bd3
Leoruiz197 <ruizruizleo@hotmail.com>
2026-04-25 11:48:11 -0300

    ajuste de timeout

--- server.js
+++ server.js
@@ -51,3 +51,8 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+setInterval(() => {
+    console.log("🔁 Verificando fila...");
+    cleanupWaitingFirst();
+}, Number(process.env.QUEUE_CHECK_INTERVAL) || 30000);
